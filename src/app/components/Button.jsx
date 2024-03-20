10753897521dc494a278a3f896b6dfb6f15356a6
kosisochukwu25 <kosisochukwuomordi@gmail.com>
2024-03-20 11:30:26 +0100

    fix: import

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button({status}) {
   return (
-    <Button ClassName='bg-slate-900 text-white rounded-md p-5 w-4/5 m-5' >{status}</Button>
+    <button ClassName='bg-slate-900 text-white rounded-md p-5 w-4/5 m-5' >{status}</button>
   )
 }
